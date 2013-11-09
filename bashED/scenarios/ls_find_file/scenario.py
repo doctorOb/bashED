12fc38670f03863f4a1d0bc87f5cc28e3cdccd13
tyler-stone <tjs_rocks@yahoo.com>
2013-11-09 01:27:28 -0500

    extended base class in example subclass

--- bashED/scenarios/ls_find_file/scenario.py
+++ bashED/scenarios/ls_find_file/scenario.py
@@ -1,13 +1,10 @@
 """this file should be the subclass which defines the prompt, how to verify the solution, the help, and the win message, maybe more stuff"""
 import os
+import base_scenario
 
 from verification_helpers import prompt, SCENARIO_DIR
-import BashED_Scenario
 
-
-class LsScenario(BashED_Scenario):
-    #this should eventually be a subclass of the main Scenario, but that doesn't exist yet...
-    #I'm looking at you Tyler Stone
+class LsScenario(base_scenario.BashED_Scenario):
 
     def __init__(self):
         self.scenario_description = ("You're a secret agent and you need to get into "
