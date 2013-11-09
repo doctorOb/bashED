62aa866e55f8c7c366c5ebf0bc9f1c5b66021f4d
Michael Perrone <mjperrone@wpi.edu>
2013-11-09 01:32:22 -0500

    patching a the driver and the scenario base class and the scenario ls_find_file implementation together

--- bashED.py
+++ bashED.py
@@ -5,19 +5,38 @@ STATE_FILE = os.path.join("bashED", "state.py")
 
 
 def play():
+    scenario = load_scenario()
+    if scenario.validate():
+        scenario.print_correct()
+        #somehow get the next scenario, load that in, and write to the STATE_FILE
+        pass
+    else:
+        print "Not quite the correct solution. Run `bashed --hint` if you need more help, and `bashed --reset` to start over."
+
+
+def hint():
+    scenario = load_scenario()
+    scenario.print_hint()
+
+
+def reset():
+    scenario = load_scenario()
+    scenario.recover()
+    pass
+
+
+def load_scenario():
     state = {}
     execfile(STATE_FILE, state)
     #current_scenario a string, initialized a bool
-
     if not state['initialized']:
         #GET THE FIRST SCENARIO OF SOME MISSION, AND THEN call the setup thingymabober
         pass
     elif not state['current_scenario']:
         raise "ERROR! The state says initialized, but there is no current_scenario set."
     else:
-        #load up the current scenario object
-        #call the "yo did it pass?" function from the current scenario
-        pass
+        return None
+        #load up the scenario defined in state['current_scenario'] and return it!
 
 
 if __name__ == '__main__':
@@ -32,6 +51,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.play:
         play()
+    elif args.hint:
+        hint()
+    elif args.reset:
+        reset()
 
 
     #need to store the current state somewhere.
