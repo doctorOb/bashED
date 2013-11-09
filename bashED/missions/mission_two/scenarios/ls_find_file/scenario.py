cd3226b82ce3f215fd135ceb7e048b2754e7c849
Michael Perrone <mjperrone@wpi.edu>
2013-11-09 12:18:43 -0500

    removingmore backticks

--- bashED/missions/mission_two/scenarios/ls_find_file/scenario.py
+++ bashED/missions/mission_two/scenarios/ls_find_file/scenario.py
@@ -8,7 +8,7 @@ class Scenario(base_scenario.Scenario):
     def __init__(self):
         self.scenario_description = ("Literally, you're boring, so just do as I say: "
                 "please just enter the phrase 'yhacksuc...cess!'"
-        self.hint = "type `ls` and see which file might contain the passwords, then type `bashed play` to continue"
+        self.hint = "type 'ls' and see which file might contain the passwords, then type 'bashed play' to continue"
         self.win_message = "Great job! You found the file!"
         self.dir = os.path.dirname(os.path.realpath(__file__))
 
