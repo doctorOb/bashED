4468fccb7ac78fd978703b704b18aba9c578511d
Tyler <tjs_rocks@yahoo.com>
2013-11-09 00:28:09 -0500

    More complete scenario base class

--- scenario.py
+++ scenario.py
@@ -8,11 +8,22 @@ class BashED_Scenario():
 		src = "setup/"
 		dest = "../../sandbox/"
 		shutil.copytree(src, dest)
-
+		
 	def checkpoint():
 		src = "../../sandbox/"
 		dest = "../../checkpoint/"
+		shutil.copytree(src, dest)
+
+	def recover():
+		src = "../../checkpoint/"
+		dest = "../../sandbox/"
 		files_to_remove = glob.glob(dest + "*")
 		for file in files_to_remove:
 			os.remove(file)
 		shutil.copytree(src, dest)
+
+	def print_prompt():
+		pass
+
+	def validate():
+		pass
