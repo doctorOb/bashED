6cf3a8d11e1a520315b9e8464fcefb8383cd3659
Michael Perrone <mjperrone@wpi.edu>
2013-11-09 12:08:00 -0500

    made a crappy storyline that isn't ambiguous

--- bashED/missions/mission_one/mission.py
+++ bashED/missions/mission_one/mission.py
@@ -4,6 +4,6 @@ import base_mission
 class Mission(base_mission.Mission):
 	
 	def __init__(self):
-		self.mission_description = "HERE'S THE FUCKING mission_description"
-		self.win_message = "HERE'S THE FUCKING win_message"
+		self.mission_description = "You have to go on your first perilous mission. today."
+		self.win_message = "You successfully navigated your first perilousmission."
 		self.dir = os.path.dirname(os.path.realpath(__file__))
--- bashED/missions/mission_two/scenarios/ls_find_file/scenario.py
+++ bashED/missions/mission_two/scenarios/ls_find_file/scenario.py
@@ -6,15 +6,11 @@ from verification_helpers import prompt
 class Scenario(base_scenario.Scenario):
 
     def __init__(self):
-        self.scenario_description = ("You're a secret agent and you need to get into "
-                                     "some system or something like that. There's a "
-                                     "file in this directory that contains the "
-                                     "password to the system. Use the `ls` command "
-                                     "to see the files in the current directory, and "
-                                     "find the files with the passwords!")
+        self.scenario_description = ("Literally, you're boring, so just do as I say: "
+                "please just enter the phrase 'yhacksuc...cess!'"
         self.hint = "type `ls` and see which file might contain the passwords, then type `bashed play` to continue"
         self.win_message = "Great job! You found the file!"
         self.dir = os.path.dirname(os.path.realpath(__file__))
 
     def validate(self):
-        return prompt("Enter the name of the secret file", "passwords.txt")
+        return prompt("What did I tell you to enter?", "yhacksuc...cess!")
