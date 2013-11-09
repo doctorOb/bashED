a32c1e8c02028acd396b97a8ddeacfacb92ca028
Mike Perrone <mperrone@kyru.us>
2013-11-09 12:18:43 -0500

    removingmore backticks

--- bashED/missions/mission_two/scenarios/ls_copy/scenario.py
+++ bashED/missions/mission_two/scenarios/ls_copy/scenario.py
@@ -7,7 +7,7 @@ class Scenario(base_scenario.Scenario):
 
     def __init__(self):
         self.scenario_description = ("Make sure a directory 'durr' exists, and in that directory is an empty file called 'df.txt'. Also, there must be a file 'f.txt', containing only the text 'a character'. Also, enter 'yale'."
-        self.hint = "'touch file.txt' creates an empty file called file.txtt. mkdir makes directories. `echo 'a string' > file.txt make the contents of file.txt be 'a string'"
+        self.hint = "'touch file.txt' creates an empty file called file.txtt. mkdir makes directories. \"echo 'a string'\" > file.txt make the contents of file.txt be 'a string'"
         self.win_message = "Great job! You did it!"
         self.dir = os.path.dirname(os.path.realpath(__file__))
 
