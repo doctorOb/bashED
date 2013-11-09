fb14ed48e975fc356df529b228169ba09fddbe38
tyler-stone <tjs_rocks@yahoo.com>
2013-11-09 16:00:36 -0500

    finished up the second mejiwowidsji

--- bashED/missions/mission_two/scenarios/rm/scenario.py
+++ bashED/missions/mission_two/scenarios/rm/scenario.py
@@ -8,7 +8,7 @@ class Scenario(base_scenario.Scenario):
     def __init__(self):
         self.scenario_description = ("THEY FOUND THE SECRET FOLDER AND NOW THEY'RE GOING TO BREAK IN. DELETE THE FOLDER NOW USING 'rm'")
         self.hint = "Use the command 'rm SECRET_CODES.txt' to delete the secret file"
-        self.win_message = "Phew. We should be safe... OH NO WAIT"
+        self.win_message = "Thank god you were here."
         self.dir = os.path.dirname(os.path.realpath(__file__))
 
     def validate(self):
